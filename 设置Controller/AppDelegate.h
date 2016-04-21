0718db14d7951f9235b37de2f39a1a410bdaf2e7
zhaohaojie <1390135920@qq.com>
2016-04-21 21:00:12 +0800

    Initial Commit

---
+++ 设置Controller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  设置Controller
+//
+//  Created by 赵浩杰 on 16/4/21.
+//  Copyright © 2016年 赵浩杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 设置ControllerTests/__ControllerTests.m
@@ -0,0 +1,39 @@
+//
+//  __ControllerTests.m
+//  设置ControllerTests
+//
+//  Created by 赵浩杰 on 16/4/21.
+//  Copyright © 2016年 赵浩杰. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface __ControllerTests : XCTestCase
+
+@end
+
+@implementation __ControllerTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
